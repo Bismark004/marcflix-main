663cf4fe13d32cdd07081f5455b538d70e4194de
Bismark Adzai <adzaibismark@gmail.com>
2023-11-24 03:56:59 +0000

    commit

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import Top from '../components/Top';
+import './Header.css';
+
+const Header = () => {
+
+    return (
+        <div>
+            <Top/>
+
+
+        </div>
+
+    )
+}
+export default Header;
