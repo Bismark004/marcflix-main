4b5aace6622a8e603234cd656c47be474325787e
Bismark Adzai <adzaibismark@gmail.com>
2023-11-24 22:52:31 +0000

    commit

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,19 +1,39 @@
+import React, {useState, useEffect} from 'react';
 import './Trending.css';
+import tmdbApi from "../Api/tmdbApi.js";
 
 function Trending ()  {
+    const [trendingMovies, setTrendingMovies] = useState([]);
+
+    useEffect(() => {
+        const fetchPopularMovies = async () => {
+            try {
+                const response = await tmdbApi.getMoviesList('popular');
+                setTrendingMovies(response.results);
+            } catch (error) {
+                console.error('Error fetching popular movies:', error);
+            }
+        };
+
+        fetchPopularMovies();
+    }, []);
     return (
         <div className="trending">
             <div className="head">
                 <h1>Trending</h1>
-                <div> <span>See More <b> ></b> </span></div>
+                <div> <span>See More <b> &gt;</b> </span></div>
                
             </div>
             <div className="grid-container">
-                <div> 1</div>
-                <div> 2</div>
-                <div> 3</div>
-                <div>4</div>
+                {trendingMovies.map((movie) => (
+                    <div key={movie.id}>
+                        <h2>{movie.title}</h2>
+
+                    </div>
+                ))
+                }
             </div>
+
 
         </div>
     )
