4112b2db0b4145261ab1c0c38ba013f9e391ea38
Bismark Adzai <adzaibismark@gmail.com>
2023-11-25 05:26:44 +0000

    commit

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,42 +1,40 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './Trending.css';
-import tmdbApi from "../Api/tmdbApi.js";
+import tmdbApi from '../Api/tmdbApi.js';
 
-function Trending ()  {
-    const [trendingMovies, setTrendingMovies] = useState([]);
+function Trending() {
+  const [trendingMovies, setTrendingMovies] = useState([]);
 
-    useEffect(() => {
-        const fetchPopularMovies = async () => {
-            try {
-                const response = await tmdbApi.getMoviesList('popular');
-                setTrendingMovies(response.results);
-            } catch (error) {
-                console.error('Error fetching popular movies:', error);
-            }
-        };
+  useEffect(() => {
+    tmdbApi.getMoviesList('popular')
+      .then(response => {
+        setTrendingMovies(response.results);
+      })
+      .catch(error => {
+        console.error('Error fetching popular movies:', error);
+      });
+  }, []);
 
-        fetchPopularMovies();
-    }, []);
-    return (
-        <div className="trending">
-            <div className="head">
-                <h1>Trending</h1>
-                <div> <span>See More <b> &gt;</b> </span></div>
-               
-            </div>
-            <div className="grid-container">
-                {trendingMovies.map((movie) => (
-                    <div key={movie.id}>
-                        <h2>{movie.title}</h2>
-
-                    </div>
-                ))
-                }
-            </div>
-
-
+  return (
+    <div className="trending">
+      <div className="head">
+        <h1>Trending</h1>
+        <div>
+          <span>See More<b>&gt;</b></span>
         </div>
-    )
-
+      </div>
+      <div className="grid-container">
+        {trendingMovies.map((movie) => (
+          <div key={movie.id}>
+            <img
+              src={`https://image.tmdb.org/t/p/w342/${movie.poster_path}`}
+              alt={movie.title}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
+
 export default Trending;
