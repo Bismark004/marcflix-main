878f8998712909b9ce0905c395bed12b13c93428
Bismark Adzai <adzaibismark@gmail.com>
2023-11-27 23:38:04 +0000

    .....

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,36 +1,19 @@
-import React, { useState, useEffect, useRef } from 'react';
-import Swiper from 'swiper';
-import 'swiper/css/swiper.css';
-import './Trending.css'; // Import your existing styles
-
+import React, { useState, useEffect } from 'react';
+import './Trending.css';
 import tmdbApi from '../Api/tmdbApi.js';
 
 function Trending() {
   const [trendingMovies, setTrendingMovies] = useState([]);
-  const swiperContainer = useRef(null);
 
   useEffect(() => {
-    const fetchTrendingMovies = async () => {
-      try {
-        const response = await tmdbApi.getMoviesList('popular');
+    tmdbApi.getMoviesList('popular')
+      .then(response => {
         setTrendingMovies(response.results);
-      } catch (error) {
+      })
+      .catch(error => {
         console.error('Error fetching popular movies:', error);
-      }
-    };
-
-    fetchTrendingMovies();
-  }, []); // Empty dependency array means this effect runs once on mount
-
-  useEffect(() => {
-    if (swiperContainer.current) {
-      // Initialize Swiper once movies are fetched
-      const mySwiper = new Swiper(swiperContainer.current, {
-        slidesPerView: 4, 
-        spaceBetween: 16, 
       });
-    }
-  }, [trendingMovies]); 
+  }, []);
 
   return (
     <div className="trending">
@@ -40,19 +23,21 @@ function Trending() {
           <span>See More<b>&gt;</b></span>
         </div>
       </div>
-      <div className="swiper-container" ref={swiperContainer}>
-        <div className="swiper-wrapper">
-          {trendingMovies.map((movie) => (
-            <div key={movie.id} className="swiper-slide">
-              <img
-                src={`https://image.tmdb.org/t/p/w342/${movie.poster_path}`}
-                className="movie-image"
-                alt={movie.title}
-              />
-            </div>
-          ))}
-        </div>
-        {/* Add pagination, navigation, etc. if needed */}
+      <div className="grid-container">
+        {trendingMovies.map((movie) => (
+          <div key={movie.id}>
+            <img
+              src={`https://image.tmdb.org/t/p/w342/${movie.poster_path}`}
+              style={{
+                width: '200px',
+                height: '200px',
+                borderRadius: '8px', // Optional: Add rounded corners
+                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', // Optional: Add a subtle shadow
+              }}
+              alt={movie.title}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
